5581ee96533d09026d30a9a6b9dbd262678157cc
AndreyKuklinov <Andreydolphin@mail.ru>
2025-11-26 15:21:43 +0300

    Arm logic

--- Assets/Scripts/MoveToTarget.cs
+++ Assets/Scripts/MoveToTarget.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoverToTarget : MonoBehaviour
+public class MoveToTarget : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform target;
+    [SerializeField] float movement_speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            target.position,
+            movement_speed * Time.deltaTime
+        );
     }
 }
---
+++ Assets/Scripts/RoverArm.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverArm : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] GameObject hand_prefab;
+    [SerializeField] float movement_speed;
+    [SerializeField] Vector2 direction;
+
+    [SerializeField] bool is_moving = false;
+
+    GameObject hand;
+
+    [ContextMenu("Start movement")]
+    public void StartMovement()
+    {
+        hand = Instantiate(hand_prefab);
+        is_moving = true;
+    }
+
+    [ContextMenu("Stop movement")]
+    public void StopMovement()
+    {
+        is_moving = false;
+    }
+
+    [ContextMenu("Destroy hand")]
+    public void DestroyHand()
+    {
+        Destroy(hand);
+    }
+
+    void Update()
+    {
+        if (is_moving)
+        {
+            hand.transform.position += (Vector3)(movement_speed * Time.deltaTime * direction);
+        }
+
+        if (hand != null)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.SetPositions(new Vector3[] {
+                transform.position,
+                hand.transform.position
+            });
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+}
