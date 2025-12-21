108d4a92e449039e99cfb3b943a2194b438b82d6
AndreyKuklinov <Andreydolphin@mail.ru>
2025-12-21 18:59:26 +0500

    Add lvl 1

---
+++ Assets/Scripts/DemoSceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemoSceneLoader : MonoBehaviour
+{
+    public string[] sceneNames;
+
+    void Update()
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+            {
+                LoadSceneByIndex(i - 1);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            LoadSceneByIndex(9);
+        }
+    }
+
+    void LoadSceneByIndex(int index)
+    {
+        if (index < 0 || index >= sceneNames.Length)
+        {
+            Debug.LogWarning($"No scene assigned for index {index}");
+            return;
+        }
+
+        SceneManager.LoadScene(sceneNames[index]);
+    }
+}
