b3647eadfa8ee8b52ba2b5e0365d59805365d8e2
AndreyKuklinov <Andreydolphin@mail.ru>
2026-06-10 13:16:04 +0300

    Implement timer

--- Assets/Scripts/Grid Objects/Old/TimedEmitter.cs
+++ Assets/Scripts/Grid Objects/Old/TimedEmitter.cs
@@ -4,89 +4,87 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimedEmitter
+public class TimedEmitter : MonoBehaviour
 {
-    //const bool RESET_WHEN_OTHER_EMITTER_TRIGGERS = true;
+    const bool RESET_WHEN_OTHER_EMITTER_TRIGGERS = true;
 
-    //[SerializeField] SignalEmitter emitter;
-    //[SerializeField] Signal[] signals;
-    //[SerializeField] float signalInterval;
-    //[SerializeField] float pauseDuration;
-    //[SerializeField] Image progressBar;
+    [SerializeField] SignalEmitter emitter;
+    [SerializeField] SignalType[] signals;
+    [SerializeField] float signalInterval;
+    [SerializeField] float pauseDuration;
+    [SerializeField] Image progressBar;
 
-    //private Coroutine timer;
-    //private int signalIndex;
+    private Coroutine timer;
+    private int signalIndex;
 
-    //protected override void Start()
-    //{
-    //    StartTimer();
-    //    Room.SignalChanged += OnSignalChanged;
-    //    base.Start();
-    //}
+    void Start()
+    {
+        StartTimer();
+        Room.SignalChanged += OnSignalChanged;
+    }
 
-    //protected override void OnDestroy()
-    //{
-    //    Room.SignalChanged -= OnSignalChanged;
-    //    base.OnDestroy();
-    //}
+    void OnDestroy()
+    {
+        Room.SignalChanged -= OnSignalChanged;
+    }
 
-    //private void OnSignalChanged(Signal obj)
-    //{
-    //    if (!RESET_WHEN_OTHER_EMITTER_TRIGGERS || progressBar.fillAmount == 1)
-    //        return;
-    //    StartTimer();
-    //}
+    private void OnSignalChanged(SignalType _)
+    {
+        if (!RESET_WHEN_OTHER_EMITTER_TRIGGERS || progressBar.fillAmount == 1)
+            return;
+        StartTimer();
+    }
 
-    //private void StartTimer()
-    //{
-    //    if (progressBar == null)
-    //        return;
+    private void StartTimer()
+    {
+        if (progressBar == null)
+            return;
 
-    //    if(signalInterval <= 0)
-    //        Debug.LogWarning("Signal Interval must be greater than 0. ", this);
+        if (signalInterval <= 0)
+            Debug.LogWarning("Signal Interval must be greater than 0. ", this);
 
-    //    progressBar.fillAmount = 0f;
+        progressBar.fillAmount = 0f;
 
-    //    if(timer != null)
-    //        StopCoroutine(timer);
+        if (timer != null)
+            StopCoroutine(timer);
 
-    //    timer = StartCoroutine(EmissionCycleRoutine());
-    //}
+        timer = StartCoroutine(EmissionCycleRoutine());
+    }
 
-    //private IEnumerator EmissionCycleRoutine()
-    //{
-    //    float timer = 0f;
+    private IEnumerator EmissionCycleRoutine()
+    {
+        float timer = 0f;
 
-    //    while (timer < signalInterval)
-    //    {
-    //        timer += Time.deltaTime;
-    //        progressBar.fillAmount = timer / signalInterval;
-    //        yield return null;
-    //    }
+        while (timer < signalInterval)
+        {
+            timer += Time.deltaTime;
+            progressBar.fillAmount = timer / signalInterval;
+            yield return null;
+        }
 
-    //    progressBar.fillAmount = 1f;
-    //    emitter.Emit(signals[signalIndex]);
-    //    yield return new WaitForSeconds(pauseDuration);
-    //    ChangeSignal();
-    //    StartTimer();
-    //}
+        progressBar.fillAmount = 1f;
+        emitter.Emit(signals[signalIndex]);
+        yield return new WaitForSeconds(pauseDuration);
+        ChangeSignal();
+        StartTimer();
+    }
 
-    //private void OnValidate()
-    //{
-    //    UpdateColor();
-    //}
+    private void OnValidate()
+    {
+        UpdateColor();
+    }
 
-    //private void UpdateColor()
-    //{
-    //    if (progressBar == null || signals == null || signals.Length == 0)
-    //        return;
+    private void UpdateColor()
+    {
+        if (progressBar == null || signals == null || signals.Length == 0)
+            return;
 
-    //    progressBar.color = SignalColor.GetColor(signals[signalIndex]);
-    //}
+        progressBar.color = SignalColor.GetColor(signals[signalIndex]);
+    }
 
-    //private void ChangeSignal()
-    //{
-    //    signalIndex = (signalIndex + 1) % signals.Length;
-    //    UpdateColor();
-    //}
+    private void ChangeSignal()
+    {
+        signalIndex = (signalIndex + 1) % signals.Length;
+        UpdateColor();
+    }
 }
