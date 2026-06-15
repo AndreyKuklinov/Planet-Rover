7875c7a3fb022d68809708c683fe2fa0018623c8
AndreyKuklinov <Andreydolphin@mail.ru>
2026-06-15 14:29:19 +0300

    Create base level

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class CameraMover : MonoBehaviour
 {
     [SerializeField] Camera cam;
-    [SerializeField] float extraPadding;
-
-    const float CELL_PADDING = 0.5f;
+    [SerializeField] float padding;
 
     void Update()
     {
@@ -24,7 +22,7 @@ public class CameraMover : MonoBehaviour
         var sizeFromWidth = (bounds.Size.x) * 0.5f / aspect;
         var targetSize = Mathf.Max(sizeFromHeight, sizeFromWidth);
 
-        targetSize += extraPadding + CELL_PADDING;
+        targetSize += padding;
 
         cam.transform.position = new Vector3(
             bounds.Center.x,
