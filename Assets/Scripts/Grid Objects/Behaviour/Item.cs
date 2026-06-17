2c37574a71de04f2438eb6eeb2fed2607925835b
AndreyKuklinov <Andreydolphin@mail.ru>
2026-06-18 02:55:50 +0300

    Move recolorable logic to Item

--- Assets/Scripts/Grid Objects/Behaviour/Item.cs
+++ Assets/Scripts/Grid Objects/Behaviour/Item.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
+using static UnityEditor.Progress;
 
-public class Item : MonoBehaviour, IHasItemData
+public class Item : MonoBehaviour, IHasItemData, IRecolorable
 {
-    [field: SerializeField] public ItemData ItemData { get; set; }
+    [field: SerializeField] public ItemData ItemData { get; private set; }
+    [SerializeField] ItemDataRepo itemDataRepo;
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    public ItemColorData CurrentColor
+        => ItemData.ColorData;
+
+    public bool CanBeRecolored(ItemColorData _)
+    {
+        return true;
+    }
+
+    public void Recolor(ItemColorData newColor)
+    {
+        var t = ItemData.TypeData;
+        var newItemData = itemDataRepo.GetItemData(t, newColor);
+        ItemData = newItemData;
+        spriteRenderer.sprite = ItemData.MainSprite;
+    }
 }
